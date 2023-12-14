18cf8eb3fe31fa1869c983ce492e57bd0f3eb87d
Yuugen <yuugenssb@proton.me>
2023-12-14 09:07:25 +0100

    refactor(term): Print a square instead of a letter for more clarity
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- src/engine/term.rs
+++ src/engine/term.rs
@@ -22,24 +22,25 @@ pub fn draw(cells: &[Vec<cells::Cell>]) -> Result<(), std::io::Error> {
 
 /// Returns the character that should represent a cell of a given [Type](game/Type).
 fn type_repr(t: &'static types::Type) -> FgDynColorDisplay<'_, Rgb, &str> {
-    match *t {
-        types::Type::Normal => "N".truecolor(168, 167, 122),
-        types::Type::Fire => "F".truecolor(238, 129, 48),
-        types::Type::Water => "W".truecolor(99, 144, 240),
-        types::Type::Grass => "G".truecolor(122, 199, 76),
-        types::Type::Electric => "E".truecolor(247, 208, 44),
-        types::Type::Ice => "I".truecolor(150, 217, 214),
-        types::Type::Fighting => "F".truecolor(194, 46, 40),
-        types::Type::Poison => "P".truecolor(163, 62, 161),
-        types::Type::Ground => "G".truecolor(226, 191, 101),
-        types::Type::Flying => "F".truecolor(169, 143, 243),
-        types::Type::Psychic => "P".truecolor(249, 85, 135),
-        types::Type::Bug => "B".truecolor(166, 185, 26),
-        types::Type::Rock => "R".truecolor(182, 161, 54),
-        types::Type::Ghost => "G".truecolor(115, 87, 151),
-        types::Type::Dark => "D".truecolor(112, 87, 70),
-        types::Type::Dragon => "D".truecolor(111, 53, 252),
-        types::Type::Steel => "S".truecolor(183, 183, 206),
-        types::Type::Fairy => "F".truecolor(214, 133, 173),
-    }
+    let (r, g, b) = match *t {
+        types::Type::Normal => (168, 167, 122),
+        types::Type::Fire => (238, 129, 48),
+        types::Type::Water => (99, 144, 240),
+        types::Type::Grass => (122, 199, 76),
+        types::Type::Electric => (247, 208, 44),
+        types::Type::Ice => (150, 217, 214),
+        types::Type::Fighting => (194, 46, 40),
+        types::Type::Poison => (163, 62, 161),
+        types::Type::Ground => (226, 191, 101),
+        types::Type::Flying => (169, 143, 243),
+        types::Type::Psychic => (249, 85, 135),
+        types::Type::Bug => (166, 185, 26),
+        types::Type::Rock => (182, 161, 54),
+        types::Type::Ghost => (115, 87, 151),
+        types::Type::Dark => (112, 87, 70),
+        types::Type::Dragon => (111, 53, 252),
+        types::Type::Steel => (183, 183, 206),
+        types::Type::Fairy => (214, 133, 173),
+    };
+    "■".truecolor(r, g, b)
 }
